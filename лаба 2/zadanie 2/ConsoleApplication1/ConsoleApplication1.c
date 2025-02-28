599a7c48efafd9637beb0628815b2da5f0d33f83
TaurineManiac <ignatladno@gmail.com>
2025-02-28 21:21:07 +0300

    Add files via upload

---
+++ лаба 2/zadanie 2/ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>
+#include<math.h>
+#include<locale.h>
+
+main() 
+{   
+	setlocale(LC_ALL, "russian");
+	int K;
+	scanf_s("%d", &K);
+
+		if (K == 1)
+		{
+			printf("Мне 1 год");
+
+		}
+		else if (K == 3 || K == 4 || K == 2)
+		{
+			printf("Мне %d ", K);
+			printf(" года");
+		}
+		else
+		{
+			printf("Мне %d", K);
+			printf(" лет");
+		}
+
+	
+
+
+
+
+}
---
+++ лаба 2/задание 1/ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+#include<locale.h>
+#include<math.h>
+int a,b,c;
+main() 
+{
+	setlocale(LC_ALL, "russian");
+	scanf_s("%d%d%d", &a, &b, &c);
+	
+	if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) 
+	{
+		printf("ДА");
+	}
+	else 
+	{
+		printf("НЕТ");
+	}
+}
